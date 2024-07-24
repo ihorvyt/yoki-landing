1302c60f8eca1a46c82b851eeb593e7df68a58ff
vlakk <lawashkalash@gmail.com>
2024-07-24 15:04:49 +0300

    fixed translate problem

--- js/translation.js
+++ js/translation.js
@@ -5,7 +5,7 @@ const resources = {
         "welcomeMessage": "Welcome to the app for finding and collaborating with like-minded people",
         "find_mates": "Find your <br> new Friends and",
         "implement_ideas": "realize your <br> ideas together",
-        "introduce_yourself" : "<h2><span class=\"yellow-text\">Tell about yourself</span> in the form, let others know who you are and what you want</h2>",
+        "introduce_yourself" : "<span class=\"yellow-text\">Tell about yourself</span> in the form, let others know who you are and what you want",
         "in_form": "in the questionnaire, let others know who you are and what you want",
         "form_details" : "<p>In the questionnaire, you can write what you like to do, your hobbies, favorite movies, music genres, and much more.</p>"
       }
@@ -16,7 +16,7 @@ const resources = {
         "welcomeMessage": "Вітаємо у застосунку для пошуку і співпраці з однодумцями",
         "find_mates": "Знайдіть своїх <br> однодумців та",
         "implement_ideas": "Реалізуйте <br>ідеї разом.",
-        "introduce_yourself": "<h2><span class=\"yellow-text\">Розкажіть про себе</span> у анкеті, дайте іншим зрозуміти хто ви і чого хочете</h2>",
+        "introduce_yourself": "<span class=\"yellow-text\">Розкажіть про себе</span> у анкеті, дайте іншим зрозуміти хто ви і чого хочете",
         "in_form": "у анкеті, дайте іншим зрозуміти хто ви і чого хочете",
         "form_details": "<p>У анкеті ви можете написати чим любите займатися, ваші захоплення, улюблені фільми, жанри музики і ще багато іншого.</p>"
       }
